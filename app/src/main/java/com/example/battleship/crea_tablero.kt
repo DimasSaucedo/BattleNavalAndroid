f4fbf01d53755cb5cbb3fb2282b4d0bf07b905b4
dayanaGlz <75285870+dayanaGlz@users.noreply.github.com>
2022-11-13 02:18:16 -0600

    funcionalidad tablero y lista
    agrege funcionalidad al botones de las vistas crea tablero y jugadoresLista, al igual que detalles de interfaz

--- app/src/main/java/com/example/battleship/crea_tablero.kt
+++ app/src/main/java/com/example/battleship/crea_tablero.kt
@@ -1,11 +1,24 @@
 package com.example.battleship
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class crea_tablero : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_crea_tablero)
+
+        val btnNext:Button=findViewById(R.id.btnNext)
+        val btnBorrar:Button=findViewById(R.id.btnNext)
+
+        btnNext.setOnClickListener{
+            val intent: Intent = Intent(this,campoBatalla::class.java)
+            startActivity(intent)
+        }
+        btnBorrar.setOnClickListener{
+            //borrar todas las naves
+        }
     }
 }
--- app/src/main/java/com/example/battleship/jugadoresLista.kt
+++ app/src/main/java/com/example/battleship/jugadoresLista.kt
@@ -1,11 +1,20 @@
 package com.example.battleship
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class jugadoresLista : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_jugadores_lista)
+
+        val btnDisenar:Button= findViewById(R.id.iniBatalla)
+
+        btnDisenar.setOnClickListener{
+            val intent: Intent = Intent(this,crea_tablero::class.java)
+            startActivity(intent)
+        }
     }
 }
