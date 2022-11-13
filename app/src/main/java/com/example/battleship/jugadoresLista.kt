a758991a83cfc4b7d0dd1006c5c961d81cd02ef1
dayanaGlz <75285870+dayanaGlz@users.noreply.github.com>
2022-11-12 22:29:28 -0600

    Vista Oponentes Activos
    se creo la vista donde se presentaran los jugadores en linea para modo  2 jugadores

---
+++ app/src/main/java/com/example/battleship/jugadoresLista.kt
@@ -0,0 +1,11 @@
+package com.example.battleship
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class jugadoresLista : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_jugadores_lista)
+    }
+}
