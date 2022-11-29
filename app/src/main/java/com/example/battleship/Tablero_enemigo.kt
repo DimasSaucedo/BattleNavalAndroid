0574045ed06afd3c4e47820d5b18ba55c71c440a
DimasSaucedo <73790380+DimasSaucedo@users.noreply.github.com>
2022-11-29 16:38:52 -0600

    Actualizacion: Funcionamiento Tablero enemigo

---
+++ app/src/main/java/com/example/battleship/Tablero_enemigo.kt
@@ -0,0 +1,11 @@
+package com.example.battleship
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Tablero_enemigo : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tablero_enemigo)
+    }
+}
