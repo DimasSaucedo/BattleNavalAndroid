5b35c7474e998009e69ce06cd3bc72ba86ce574a
DimasSaucedo <73790380+DimasSaucedo@users.noreply.github.com>
2022-12-12 13:42:32 -0600

    Deberia jalar aqui, ya me jalo

---
+++ app/src/main/java/claseFirebase/firebaseConexiones.kt
@@ -0,0 +1,4 @@
+package claseFirebase
+
+class firebaseConexiones {
+}
--- app/src/main/java/com/example/battleship/Conexion.kt
+++
@@ -1,9 +0,0 @@
-package com.example.battleship
-
-class Conexion {
-    var ip:String = "156.67.74.151"
-    var puerto = 7
-
-    constructor(){
-    }
-}
